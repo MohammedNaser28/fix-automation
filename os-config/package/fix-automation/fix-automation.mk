4949179b857a2641457a7e3e72c8541578177b3f
Mohammed Naser <MohammedNaser2826@gmai.com>
2026-05-18 01:32:26 +0300

    Remove our init system and replace with busybox

--- os-config/package/fix-automation/fix-automation.mk
+++ os-config/package/fix-automation/fix-automation.mk
@@ -16,21 +16,6 @@ define FIX_AUTOMATION_INSTALL_TARGET_CMDS
 	$(INSTALL) -D -m 0755 \
 		$(BR2_EXTERNAL_LINUX_RESCUE_PATH)/../target/x86_64-unknown-linux-musl/release/fix-automation \
 		$(TARGET_DIR)/usr/bin/fix-automation
-
-	# Set as PID 1 with environment setup
-	echo '#!/bin/sh' > $(TARGET_DIR)/sbin/init
-	echo 'export PATH=/sbin:/bin:/usr/sbin:/usr/bin' >> $(TARGET_DIR)/sbin/init
-	echo 'export TERM=linux' >> $(TARGET_DIR)/sbin/init
-	echo 'echo "Mounting virtual filesystems..."' >> $(TARGET_DIR)/sbin/init
-	echo 'mount -t proc proc /proc' >> $(TARGET_DIR)/sbin/init
-	echo 'mount -t sysfs sys /sys' >> $(TARGET_DIR)/sbin/init
-	echo 'mount -t devtmpfs dev /dev' >> $(TARGET_DIR)/sbin/init
-	echo 'echo "Starting Fix-Automation Rescue Environment..."' >> $(TARGET_DIR)/sbin/init
-	echo '/usr/bin/fix-automation' >> $(TARGET_DIR)/sbin/init
-	echo 'echo "Exiting... rebooting system in 3 seconds."' >> $(TARGET_DIR)/sbin/init
-	echo 'sleep 3' >> $(TARGET_DIR)/sbin/init
-	echo 'reboot -f' >> $(TARGET_DIR)/sbin/init
-	chmod +x $(TARGET_DIR)/sbin/init
 endef
 
 $(eval $(generic-package))
